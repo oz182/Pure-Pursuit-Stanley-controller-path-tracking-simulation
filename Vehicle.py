24ad58f6d1e67cdb433221f14347e9d608ce5f55
ozelbaz <oz.elbaz@gmail.com>
2024-06-19 22:24:01 +0300

    Added coordinates transformation functions

--- Vehicle.py
+++ Vehicle.py
@@ -31,3 +31,17 @@ class Vehicle:
     def set_speed(self, speed):
         """Set the vehicle speed."""
         self.v = speed
+
+    def global_to_ego(self, X_global, Y_global):
+        """Transform global coordinates to ego coordinates."""
+        dx = X_global - self.x
+        dy = Y_global - self.y
+        x_ego = np.cos(self.psi) * dx + np.sin(self.psi) * dy
+        y_ego = -np.sin(self.psi) * dx + np.cos(self.psi) * dy
+        return x_ego, y_ego
+
+    def ego_to_global(self, x_ego, y_ego):
+        """Transform ego coordinates to global coordinates."""
+        X_global = self.x + np.cos(self.psi) * x_ego - np.sin(self.psi) * y_ego
+        Y_global = self.y + np.sin(self.psi) * x_ego + np.cos(self.psi) * y_ego
+        return X_global, Y_global
