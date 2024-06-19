1178dad6c91bf8e3b22048c61233eb974e212a62
ozelbaz <oz.elbaz@gmail.com>
2024-06-20 01:37:14 +0300

    Updated README file, started the tracking algo

--- main.py
+++ main.py
@@ -6,18 +6,16 @@ from PurePursuitAlgo import *
 
 
 def main():
-    TargetPath = path(PathLength=50)
+    TargetPath = path(PathLength=100)
     Vehicle = vehicle(x0=0.1,y0=0.1,psi0=0.0,v0=1.0)
     TrackingAlgo = PurePursuitAlgo(lookahead_distance=2.0)
 
     while(True):
         simulation(TargetPath, Vehicle)
         #SteeringCommand = TrackingAlgo.calculate_lookahead_point(TargetPath,Vehicle)
-        SteeringCommand = 10
-        Vehicle.set_steering_angle(SteeringCommand)
-        Vehicle.update(0.2)
-        print(Vehicle.pos)
-
+        #Vehicle.set_steering_angle(SteeringCommand)
+        Vehicle.set_steering_angle(3.14)
+        Vehicle.update(delta_t=0.2)
 
 
 if __name__ == "__main__":
