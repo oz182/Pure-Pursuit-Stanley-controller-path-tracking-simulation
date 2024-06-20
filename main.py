08598174045e16e0f171e6d841062d575cbaf596
ozelbaz <oz.elbaz@gmail.com>
2024-06-20 20:00:17 +0300

    Simulation is working, need to fix tracking

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from PurePursuitAlgo import *
 def main():
     TargetPath = path(PathLength=100)
     Vehicle = vehicle(x0=0.1,y0=0.1,psi0=0.0,v0=1.0)
-    TrackingAlgo = PurePursuitAlgo(lookahead_distance=2.0)
+    TrackingAlgo = PurePursuitAlgo(TargetPath, Vehicle, lookahead_distance=1.5)
 
     while(True):
         simulation(TargetPath, Vehicle)
-        SteeringCommand = TrackingAlgo.calculate_lookahead_point(TargetPath,Vehicle)
+        SteeringCommand = TrackingAlgo.calculate_steering_angle()
         Vehicle.set_steering_angle(SteeringCommand)
-        #Vehicle.set_steering_angle(3.14)
+        print(SteeringCommand)
         Vehicle.update(delta_t=0.2)
 
 
